f842c1c167f3968e7b268147e8e3ff63850238ff
benaiahss <benaiahs59@gmail.com>
2023-01-12 11:28:21 -0600

    nav bar shadow

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
 import NavBar from './components/NavBar';
+import "../src/components/Pages/SignUp"
+import SignUp from '../src/components/Pages/SignUp';
 
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <SignUp/>
     </div>
   );
 }
